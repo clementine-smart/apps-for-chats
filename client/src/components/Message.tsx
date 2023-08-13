7bc894d5110916cdb8b1307b2df01064456d1b08
Clementine-Smart <cmwsmart@gmail.com>
2023-08-13 13:51:51 +1200

    Fix: remove viewUserCount

--- client/src/components/Message.tsx
+++ client/src/components/Message.tsx
@@ -7,11 +7,9 @@ interface Props {
 
 function Message({ message }: Props) {
   const [viewUser, setViewUser] = useState(false)
-  const [viewUserCount, setViewUserCount] = useState(0)
 
   function handleClick() {
     setViewUser((viewUser) => !viewUser)
-    setViewUserCount((viewUserCount) => viewUserCount + 1)
   }
 
   return (
@@ -19,12 +17,10 @@ function Message({ message }: Props) {
       <p
         onClick={handleClick}
         className={`${message.from === 'me' ? 'bg-red-700' : 'bg-green-500'} ${
-          viewUser && viewUserCount < 3 && '!bg-zinc-800 !text-zinc-400'
+          viewUser && '!bg-zinc-800 !text-zinc-400'
         } p-2 w-fit rounded-lg cursor-pointer`}
       >
-        {viewUser && viewUserCount < 3
-          ? `from: ${message.user}`
-          : message.message}
+        {viewUser ? `from: ${message.user}` : message.message}
       </p>
     </div>
   )
